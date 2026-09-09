74363483c681e048240061c9c91c02c796885f38
ashxom <anseha090115@naver.com>
2026-09-09 10:27:27 +0900

    feat :: 한걸음 브랜드 색상 토큰 정의

--- app/src/main/java/com/example/one_step/ui/theme/Color.kt
+++ app/src/main/java/com/example/one_step/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.one_step.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val OneStepBlue = Color(0xFF004AC6)
+val OneStepBlueBright = Color(0xFF2563EB)
+val OneStepBlueSoft = Color(0xFFEAEDFF)
+val OneStepBlueMuted = Color(0xFFDBE1FF)
+val OneStepBackground = Color(0xFFFAF8FF)
+val OneStepSurface = Color(0xFFFFFFFF)
+val OneStepText = Color(0xFF131B2E)
+val OneStepTextMuted = Color(0xFF434655)
+val OneStepSuccess = Color(0xFF006C49)
+val OneStepSuccessSoft = Color(0xFFDCF8EB)
+val OneStepBorder = Color(0xFFE8E8F0)
